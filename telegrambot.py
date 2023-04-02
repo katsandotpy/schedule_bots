8772797bbeebe6b59cf5f1b04e04c3e73d2741d0
kabantop <77514108+kabantop@users.noreply.github.com>
2023-04-02 17:58:46 +0300

    телега
    письки

---
+++ telegrambot.py
@@ -0,0 +1,29 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+TOKEN = '6109872161:AAEGnoPlbSjftA8CKx9xS0Y8LIBnPTE6DJ4'
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет! Я - робот долбоёб!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Для получения расписания введите 'дай расписание чмо' (без кавычек)")
+
+
+@dp.message_handler()
+async def file_message(msg: 'дай расписание чмо'):
+    await msg.reply("Кому расписание, а кому хуев сосание, пошел нахуй")
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
